144e2057890c796e619a74967383bf77d8050054
Jeremy Pryor <jeremypryor16@gmail.com>
2022-07-07 09:59:57 -0700

    week 2 cleaning

--- week2/routes/movieRouter.js
+++ week2/routes/movieRouter.js
@@ -27,17 +27,17 @@ movieRouter.post("/", (req, res, next) => {
 })
 
 
-// Get One
-movieRouter.get("/:movieId", (req, res, next) => {
-  const movieId = req.params.movieId
-  const foundMovie = Movie.find(movie => movie._id === movieId)
-  if(!foundMovie){
-    const error = new Error(`The item with id ${movieId} was not found.`)
-    res.status(500)
-    return next(error)
-  }
-  return res.status(200).send(foundMovie)
-})
+// Get One -- Does not Work
+// movieRouter.get("/:movieId", (req, res, next) => {
+//   const movieId = req.params.movieId
+//   const foundMovie = Movie.find(movie => movie._id === movieId)
+//   if(!foundMovie){
+//     const error = new Error(`The item with id ${movieId} was not found.`)
+//     res.status(500)
+//     return next(error)
+//   }
+//   return res.status(200).send(foundMovie)
+// })
 
 
 // Get by genre
@@ -51,7 +51,6 @@ movieRouter.get("/search/genre", (req, res, next) => {
   const filteredMovies = movies.filter(movie => movie.genre === genre)
   return res.status(200).send(filteredMovies)
 })
-
 
 // Delete One
 movieRouter.delete("/:movieId", (req, res, next) => {
